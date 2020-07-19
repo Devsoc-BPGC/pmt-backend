64b0e43fe9ea0b30ca3fb36042cc55083404d5ad
Vaibhav Ganatra <vaib2641@gmail.com>
2020-07-19 13:42:55 +0530

    Added Security middleware

--- pmt-auth/src/Express.ts
+++ pmt-auth/src/Express.ts
@@ -5,62 +5,62 @@
  */
 
 import * as express from 'express';
-import SecurityMiddleWare from './middleware/SecurityMiddleWare'
+import SecurityMiddleWare from './middleware/SecurityMiddleWare';
 
 class Express {
-    /**
-     * Create express app
-     */
+	/**
+	 * Create express app
+	 */
 
-    public express: express.Application; // express.Application indicates the interface type of express app.
+	public express: express.Application; // express.Application indicates the interface type of express app.
 
-    constructor() {
-        this.express = express();
+	constructor() {
+		this.express = express();
 
-        this.mountEnv();
-        this.mountMiddleware(this.express);
-        this.mountRoutes();
-    }
+		this.mountEnv();
+		this.mountMiddleware(this.express);
+		this.mountRoutes();
+	}
 
-    /**
-     * mountEnv: For loading env
-     */
-    private mountEnv(): void {
-        // Load the env file here.
-    }
+	/**
+	 * mountEnv: For loading env
+	 */
+	private mountEnv(): void {
+		// Load the env file here.
+	}
 
-    /**
-     * mountMiddleware: For adding middlewares
-     */
-    private mountMiddleware(express:express.Application): void {
-        // Load your middlewares here
-        SecurityMiddleWare.apply(express);
-    }
+	/**
+	 * mountMiddleware: For adding middlewares
+	 */
+	private mountMiddleware(express: express.Application): void {
+		// Load your middlewares here
+		express = new SecurityMiddleWare().apply(express);
+	}
 
-    /**
-     * mountRoutes: For adding routes
-     */
-    private mountRoutes(): void {
-        // Load your routes here
-    }
+	/**
+	 * mountRoutes: For adding routes
+	 */
+	private mountRoutes(): void {
+		// Load your routes here
+	}
 
-    /**
-     * mountErrors: For handling errors/exceptions
-     */
-    public mountErrors() {
-        // Load your error/exceptions here
-    }
+	/**
+	 * mountErrors: For handling errors/exceptions
+	 */
+	public mountErrors() {
+		// Load your error/exceptions here
+	}
 
-    /**
-     * init
-     */
-    public init(PORT: string): void {
-        const port = PORT;
+	/**
+	 * init
+	 */
+	public init(PORT: string): void {
+		const port = PORT;
 
-        this.express.listen(port, (): void => {
-            console.log(`The server is listening on PORT:${port}`);
-        });
-    }
+		this.express.listen(port, (): void => {
+			console.log(`The server is listening on PORT:${port}`);
+		});
+	}
 }
 
 export default new Express();
--- pmt-auth/src/middleware/SecurityMiddleWare.ts
+++ pmt-auth/src/middleware/SecurityMiddleWare.ts
@@ -1,33 +1,29 @@
-import * as cors from "cors";
-import { Application } from "express";
-import * as lusca from "lusca";
+import * as cors from 'cors';
+import { Application } from 'express';
+import * as lusca from 'lusca';
 
-class CORS {
-	static mount(express: Application): Application {
-		express.use(cors());		//Enables cors
+class SecurityMiddleWare {
+	private defaultCors(express: Application): Application {
+		express.use(cors()); 				// Enables cors
 		return express;
 	}
-}
-class LUSCA {
-	static mount(express: Application): Application {
+
+	private lusca(express: Application): Application {
 		express.use(
 			lusca({
-				xssProtection:true, 	 //Prevents Cross-Site scripting
-				nosniff: true, 		 //Prevents MIME-sniffing
-				xframe: 'SAMEORIGIN', //Clickjacking
-				
+				xssProtection: true, 		// Prevents Cross-Site scripting
+				nosniff: true, 				// Prevents MIME-sniffing
+				xframe: 'SAMEORIGIN' 		// Prevents Clickjacking
 			})
 		);
 		return express;
 	}
-}
-    
-class applySecurityMiddleWares {
-    public static apply(express: Application): Application {
-        express = CORS.mount(express);
-        express=LUSCA.mount(express)
+
+	public apply(express: Application): Application {
+		express = this.defaultCors(express);
+		express = this.lusca(express);
 		return express;
 	}
 }
 
-export default applySecurityMiddleWares
+export default SecurityMiddleWare;
