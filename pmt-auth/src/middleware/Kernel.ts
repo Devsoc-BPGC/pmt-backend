c5990252b3ebebd7f3d26b3cd7746201eef22300
grv1 <gauravdotiya@gmail.com>
2020-07-21 19:44:36 +0530

    Basic passport setup with github

--- pmt-auth/src/middleware/Kernel.ts
+++ pmt-auth/src/middleware/Kernel.ts
@@ -4,28 +4,32 @@
  * @author Sarvesh Shinde <SarveshShinde64@gmail.com
  */
 
-import Http from './Http';
-import Security from './Security';
+import Http from "./Http";
+import Security from "./Security";
+import Passport from "./Passport";
 
-import { Application } from 'express';
+import { Application } from "express";
 
 class Kernel {
-	public security: Security;
-	public express: Application;
-	public http: Http;
+  public security: Security;
+  public express: Application;
+  public http: Http;
+  public passport: Passport;
 
-	constructor(__express: Application) {
-		this.security = new Security(__express);
-		this.http = new Http(__express);
-		this.express = __express;
-	}
+  constructor(__express: Application) {
+    this.security = new Security(__express);
+    this.http = new Http(__express);
+    this.express = __express;
+    this.passport = new Passport();
+  }
 
-	public init (): Application {
-		this.express = this.security.apply();
-		this.express = this.http.mount();
+  public init(): Application {
+    this.express = this.security.apply();
+    this.express = this.passport.init();
+    this.express = this.http.mount();
 
-		return this.express;
-	}
+    return this.express;
+  }
 }
 
 export default Kernel;
