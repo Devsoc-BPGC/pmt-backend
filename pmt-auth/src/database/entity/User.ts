b1294ecdc98e2fd1b659b8f25cbe6cff5bf8512f
ritvij14 <ritvij2001@gmail.com>
2020-07-23 15:10:17 +0530

    Completed User entitiy model

--- pmt-auth/src/database/entity/User.ts
+++ pmt-auth/src/database/entity/User.ts
@@ -4,15 +4,59 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 export class Users {
 
 	@PrimaryGeneratedColumn()
-	id: number | undefined;
+    id: number | undefined;
+    
+    @Column('text')
+    login: string | undefined;
+
+    @Column('text')
+    avatar_url: string | undefined;
+
+    @Column('text')
+    html_url: string | undefined;
+
+    @Column('text')
+    repos_url: string | undefined;
 
 	@Column('text')
-	firstName: string | undefined;
+	name: string | undefined;
 
 	@Column('text')
-	lastName: string | undefined;
+    company: string | undefined;
+    
+    @Column('text')
+    blog: string | undefined;
+    
+    @Column('text')
+    location: string | undefined;
+    
+    @Column('text')
+    email: string | undefined;
+    
+    @Column('text')
+    bio: string | undefined;
+    
+    @Column('text')
+    twitter_username: string | undefined;
 
 	@Column('int')
-	age: number | undefined;
+    public_repos: number | undefined;
 
+    @Column('int')
+    public_gists: number | undefined;
+
+    @Column('int')
+    followers: number | undefined;
+
+    @Column('int')
+    following: number | undefined;
+
+    @Column('int')
+    private_gists: number | undefined;
+
+    @Column('int')
+    total_private_repos: number | undefined;
+
+    @Column('int')
+    owned_private_repos: number | undefined;
 }
