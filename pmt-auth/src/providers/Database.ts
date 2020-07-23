42510d798a51b66357425ca02943d3d1d64a9fcf
Sarvesh67 <SarveshShinde64@gmail.com>
2020-07-23 14:49:11 +0530

    Added travis yml file for CI
    1. Added script for test and lint in package json.

--- pmt-auth/src/providers/Database.ts
+++ pmt-auth/src/providers/Database.ts
@@ -1,12 +1,23 @@
+/**
+ * Initialise tyeorm.
+ *
+ * @author Sarvesh Shinde <SarveshShinde64@gmail.com>
+ */
+
 import 'reflect-metadata';
 import {createConnection, Connection} from 'typeorm';
-import {Users} from '../database/entity/User';
+// import { Users } from '../database/entity/User';
 
 export default class Database {
     public static async init (): Promise<any> {
         try {
             const connection: Connection = await createConnection();
             console.log('Hey, the database is listening on PORT: 5432');
+            /* let user = new Users;
+            user.firstName = 'Sarvesh';
+            user.age = 20;
+            user.lastName = 'Shinde';
+            await connection.manager.save(user); */
             return connection;
         } catch (err) {
             // Log the error here.
