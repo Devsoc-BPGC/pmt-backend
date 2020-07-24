7424f87fe4deb0f849599659200d28bcc305a280
RickyPatel <54434481+RickyPatel@users.noreply.github.com>
2020-07-25 00:20:50 +0530

    Added middleware which serves static files

---
+++ pmt-auth/src/middleware/Statics.ts
@@ -0,0 +1,35 @@
+/**
+ * Serving static files
+ *
+ * @author DevSoc
+ */
+
+import * as path from 'path';
+import * as express from 'express';
+import { Application } from 'express';
+
+// import Log from './Log';
+
+class Statics {
+	public express: Application;
+
+	constructor(__express: Application) {
+		this.express = __express;
+	}
+
+	public mount(): Application {
+		
+		console.log('Booting the Statics middleware...');
+
+		// Load Statics
+		this.express.use(express.static(path.join(__dirname, '../../../public')));
+
+		// Load NPM Statics
+		// this.express.use(express.static(path.join(__dirname, '../../node_modules')));
+	
+		return this.express;
+	}
+    
+}
+
+export default Statics;
