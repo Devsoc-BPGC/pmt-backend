5727ae04e3ef828db2b0942f07ffdb2cd144c52d
ritvij14 <ritvij2001@gmail.com>
2020-08-06 18:27:24 +0530

    Added interface for Github Details

---
+++ src/interfaces/models/GithubDetails.ts
@@ -0,0 +1,24 @@
+import { User } from './User';
+
+export interface GithubDetails {
+    user?: User;
+    html_url: string;
+    type: string;
+    site_admin: boolean;
+    company?: string;
+    blog?: string;
+    location?: string;
+    bio?: string;
+    hireable?: boolean;
+    twitter_username?: string;
+    followers: number;
+    following: number;
+    public_repos?: number;
+    public_gists?: number;
+    private_gists?: number;
+    total_private_repos?: number;
+    owned_private_repos?: number;
+    collaborators?: number;
+    two_factor_authentication?: boolean;
+    organizations_url?: string;
+}
