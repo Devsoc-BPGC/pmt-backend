f5a502dc5c9a2ff5275ea17f636ec9a8451682f5
Vaibhav Ganatra <vaib2641@gmail.com>
2021-01-23 04:49:18 +0530

    Modefied mutation and queries related to Cards

--- src/resolvers/CardResolver.ts
+++ src/resolvers/CardResolver.ts
@@ -1,3 +1,4 @@
+import { type } from 'os';
 import {
 	Arg,
 	Field,
@@ -30,10 +31,47 @@ class CardInput {
 	created_by_id!: number;
 
 	@Field(() => [String], { nullable: true })
-	labels!: string[];
+	labels?: string[];
+
+	@Field(() => [String], { nullable: true })
+	label_colors?: string[];
+
+	@Field(() => [String], { nullable: true })
+	tasks?: string[];
+
+	@Field(() => [Boolean], { nullable: true })
+	task_status?: boolean[];
+
+	// Date format to be used: YYYY/MM/DD HH:mm:ss
+	@Field()
+	deadline!: string;
 
 	@Field()
-	deadline!: Date;
+	background!: string;
+}
+
+@InputType()
+class EditCardInput{
+	@Field(() => String)
+	title!: string;
+
+	@Field(() => String)
+	description!: string;
+
+	@Field(() => [String])
+	labels!: string[];
+
+	@Field(() => [String])
+	label_colors!: string[];
+
+	@Field(() => [String])
+	tasks!: string[];
+
+	@Field(() => [Boolean])
+	task_status!: boolean[];
+
+	@Field(() => String)
+	deadline!: string;
 }
 
 @Resolver(Card)
@@ -42,36 +80,44 @@ export class CardResolver {
 	UserRepo = getCustomRepository(UserRepository);
 
 	@Mutation((type) => Card)
-	async addCard(
+	async AddCard(
 		@Arg('card_data')
 			{
 				title,
 				description,
 				board_id,
 				labels,
+				label_colors,
+				tasks,
+				task_status,
 				created_by_id,
-				deadline
+				deadline,
+				background
 			}: CardInput
 	): Promise<Card> {
 		const user = await this.UserRepo.findOne(created_by_id);
 		const board = await getCustomRepository(TaskboardRepository).findOne(
 			board_id
 		);
-
+		task_status = Array(tasks?.length).fill(false);
 		const card = this.CardRepo.create({
 			title: title,
 			description: description,
 			deadline: deadline,
 			created_by: user,
 			board: board,
-			labels: labels
+			labels: labels,
+			label_colors: label_colors,
+			tasks: tasks,
+			task_status: task_status,
+			background: background
 		});
 		await this.CardRepo.save(card);
 		return card!;
 	}
 
 	@Query((returns) => Card)
-	async cardInfo(@Arg('card_id') cardId: number): Promise<Card> {
+	async CardInfo(@Arg('card_id') cardId: number): Promise<Card> {
 		const card = await this.CardRepo.findOne(cardId);
 		return card!;
 	}
@@ -101,7 +147,7 @@ export class CardResolver {
 	}
 
 	@Mutation((type) => Card)
-	async addMemberToCard(
+	async AddMemberToCard(
 		@Arg('card_id') cardId: number,
 			@Arg('user_id') userId: number
 	): Promise<Card> {
@@ -111,7 +157,7 @@ export class CardResolver {
 	}
 
 	@Mutation((type) => Card)
-	async chageCardStatusToOngoing(
+	async ChageCardStatusToOngoing(
 		@Arg('card_id') cardId: number
 	): Promise<Card> {
 		const card = await this.CardRepo.findOne(cardId);
@@ -120,16 +166,43 @@ export class CardResolver {
 	}
 
 	@Mutation((type) => Card)
-	async chageCardStatusToCompleted(
+	async ChageCardStatusToCompleted(
 		@Arg('card_id') cardId: number
 	): Promise<Card> {
 		const card = await this.CardRepo.findOne(cardId);
 		if (card?.card_status === CardStatus.COMPLETED) {
 			return card;
 		} else {
-			card!.completed_at = new Date();
+			card!.completed_at = new Date().toISOString();
 			await this.CardRepo.changeCardStatus(card!, CardStatus.COMPLETED);
 			return card!;
+		}
+	}
+
+	@Mutation((type) => Card)
+	async EditCard(
+		@Arg('card_id') card_id: number,
+			@Arg('card_info') {
+				title,
+				description,
+				labels,
+				label_colors,
+				tasks,
+				task_status,
+				deadline
+			}: EditCardInput
+	): Promise<Card | undefined> {
+		const card = await this.CardRepo.findOne(card_id);
+		if (card) {
+			card.title = title;
+			card.description = description;
+			card.tasks = tasks;
+			card.task_status = task_status;
+			card.labels = labels;
+			card.label_colors = label_colors;
+			card.deadline = deadline;
+			await this.CardRepo.save(card);
+			return card;
 		}
 	}
 
